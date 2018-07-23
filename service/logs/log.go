5c35a05c77b0a6903dc886b74995abf27081a158
JimYJ <yblues@hotmail.com>
2018-07-23 12:35:09 +0800

    优化日志中间件

--- service/logs/log.go
+++ service/logs/log.go
@@ -1,13 +1,10 @@
 package log
 
 import (
-	"bytes"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"io"
-	"io/ioutil"
 	"log"
-	"net/http/httputil"
 	"os"
 	"runtime"
 	"time"
@@ -76,75 +73,10 @@ func Logs() gin.HandlerFunc {
 	}
 }
 
-// Recovery 异常日志分割
-func Recovery() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		defer func() {
-			if err := recover(); err != nil {
-				stack := stack(3)
-				httprequest, _ := httputil.DumpRequest(c.Request, false)
-				Errorf("[Recovery] %s panic recovered:\n%s\n%s\n%s%s", time.Now().Format("2006/01/02 - 15:04:05"), string(httprequest), err, stack, reset)
-				c.AbortWithStatus(500)
-			}
-		}()
-		c.Next()
-	}
-}
-
-func stack(skip int) []byte {
-	buf := new(bytes.Buffer) // the returned data
-	// As we loop, we open files and read them. These variables record the currently
-	// loaded file.
-	var lines [][]byte
-	var lastFile string
-	for i := skip; ; i++ { // Skip the expected number of frames
-		pc, file, line, ok := runtime.Caller(i)
-		if !ok {
-			break
-		}
-		// Print this much at least.  If we can't find the source, it won't show.
-		fmt.Fprintf(buf, "%s:%d (0x%x)\n", file, line, pc)
-		if file != lastFile {
-			data, err := ioutil.ReadFile(file)
-			if err != nil {
-				continue
-			}
-			lines = bytes.Split(data, []byte{'\n'})
-			lastFile = file
-		}
-		fmt.Fprintf(buf, "\t%s: %s\n", function(pc), source(lines, line))
-	}
-	return buf.Bytes()
-}
-
-func function(pc uintptr) []byte {
-	fn := runtime.FuncForPC(pc)
-	if fn == nil {
-		return dunno
-	}
-	name := []byte(fn.Name())
-
-	if lastslash := bytes.LastIndex(name, slash); lastslash >= 0 {
-		name = name[lastslash+1:]
-	}
-	if period := bytes.Index(name, dot); period >= 0 {
-		name = name[period+1:]
-	}
-	name = bytes.Replace(name, centerDot, dot, -1)
-	return name
-}
-func source(lines [][]byte, n int) []byte {
-	n-- // in stack trace, lines are 1-indexed but our array is 0-indexed
-	if n < 0 || n >= len(lines) {
-		return dunno
-	}
-	return bytes.TrimSpace(lines[n])
-}
-
 // Println 输出日常日志
 func Println(args ...interface{}) {
-	funcName, _, line, _ := runtime.Caller(1)
-	str := fmt.Sprintf("%s%s%d", runtime.FuncForPC(funcName).Name(), ":", line)
+	_, filePath, line, _ := runtime.Caller(1)
+	str := fmt.Sprintf("%s%s%d", filePath, ":", line)
 	a := []interface{}{str}
 	a = append(a, args...)
 	logFile.info2.Println(a...)
